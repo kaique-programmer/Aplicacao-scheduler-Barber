5503ed6f1a0f91414e700317dbb9c2608d77ded4
kaique oliveira santos <kaiquevicio@gmail.com>
2020-07-24 14:13:26 -0300

    Implementando banco de dados

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'ok'});
+  return res.json({ message: 'hlo'});
 });
 
 export default routes;
