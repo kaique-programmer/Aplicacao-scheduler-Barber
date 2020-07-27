0b4dc0cb1c277d90e854b954c530843f34f9dc09
kaique oliveira santos <kaiquevicio@gmail.com>
2020-07-27 14:06:41 -0300

    Listagem de agendamento de prestador finalizado

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleControlller from './app/controllers/ScheduleControlller';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -24,6 +25,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleControlller.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
