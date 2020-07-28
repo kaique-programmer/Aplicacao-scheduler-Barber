eaa993c37a6f983179f5ab6a21999ad5d174d0d9
kaique oliveira santos <kaiquevicio@gmail.com>
2020-07-28 18:32:09 -0300

    Notifications implementada

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,10 +12,11 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
-    // Método responsável pela conexão com o base de dados e carregar os models
+    // Método responsável pela conexão com a base de dados e carregar os models
     this.connection = new Sequelize(databaseConfig);
 
     models
@@ -22,6 +24,17 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import app from './app';
 
-app.listen(3535);
+app.listen(3636);
