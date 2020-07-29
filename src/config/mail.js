876e1dfc69d4bf93fd5824e68c957051a06bc737
kaique oliveira santos <kaiquevicio@gmail.com>
2020-07-29 16:33:31 -0300

    Envio de email implementado

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2cb9d249ac3273',
+    pass: '52fd8af5d5c900',
+  },
+  default: {
+    from: 'Team GoBarber <noreply@gobarber.com>',
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import app from './app';
 
-app.listen(3636);
+app.listen(3737);
